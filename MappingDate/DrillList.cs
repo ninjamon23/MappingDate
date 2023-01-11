90ab3f4fbbff60b0c333f2938650ef919581a05a
ninjamon23 <58360160+ninjamon23@users.noreply.github.com>
2023-01-12 01:52:46 +0800

    first commit

---
+++ MappingDate/DrillList.cs
@@ -0,0 +1,86 @@
+﻿namespace MappingDate
+{
+    public class DrillList
+    {
+        public string TagReferenceId { get; set; }
+        public string ScheduleTaskId { get; set; }
+        public List<DrillItem> DrillItems { get; set; }
+
+        public static List<DrillList> GetOpenDrills()
+        {
+            return new List<DrillList> { new DrillList
+            {
+                TagReferenceId = "tag fire drill",
+                ScheduleTaskId = "schedule 1",
+                DrillItems = new List<DrillItem> { new DrillItem
+                    {
+                        Title = "Fire Drill",
+                        DueDate = new DateTime(2022,3,10)
+                    },
+                        new DrillItem
+                    {
+                        Title = "Fire Drill",
+                        DueDate = new DateTime(2022,4,10)
+                    }
+                }
+            }
+            };
+        }
+        public static List<DrillList> GetClosedDrills()
+        {
+            return new List<DrillList> { new DrillList
+{
+    TagReferenceId = "tag fire drill",
+    ScheduleTaskId = "schedule 1",
+    DrillItems = new List<DrillItem> { new DrillItem
+        {
+            Title = "Fire Drill",
+            CompletedDate = new DateTime(2022,1,11),
+            LinkId = "form submission "
+        },
+        new DrillItem
+        {
+            Title = "Fire Drill",
+            CompletedDate = new DateTime(2022,2,10)
+        }
+    }
+}
+            };
+        }
+    }
+
+    public class DrillItem
+    {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public string LinkId { get; set; }
+    }
+
+    public class ScheduleTask
+    {
+        public string ScheduleId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Interval SchedInterval { get; set; }
+
+        public enum Interval
+        {
+            Daily,
+            Weekly,
+            Monthly,
+            Yearly
+        }
+
+        public static ScheduleTask GetSchedule()
+        {
+            return new ScheduleTask
+            {
+                SchedInterval = ScheduleTask.Interval.Monthly,
+                StartDate = new DateTime(2022, 1, 10),
+                EndDate = new DateTime(2023, 1, 10),
+                ScheduleId = "schedule 1"
+            };
+        }
+    }
+}
---
+++ MappingDate/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MappingDate;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var schedule = ScheduleTask.GetSchedule();
+
+var openDrills = DrillList.GetOpenDrills();
+
+var closedDrills = DrillList.GetClosedDrills();
+
+foreach (var closedDrill in closedDrills)
+{
+    var drillToCheck = openDrills.Where(c => c.TagReferenceId == closedDrill.TagReferenceId).FirstOrDefault();
+    if (drillToCheck == null)
+        continue;
+
+    var openDrillNewestDueDate = drillToCheck.DrillItems.OrderBy(c => c.DueDate).Select(c => c.DueDate).FirstOrDefault();
+
+    var processDate = schedule.StartDate;
+    var usedTicks = new List<long>();
+    while (processDate < openDrillNewestDueDate)
+    {
+        var processingDrillItems = closedDrill.DrillItems;
+        Console.WriteLine($"Processing {processDate}");
+        var nearestDiff = processingDrillItems.Where(c => !usedTicks.Contains(Math.Abs((c.CompletedDate.Value - processDate).Ticks)))
+            .Min(c => Math.Abs((c.CompletedDate.Value - processDate).Ticks));
+        var nearest = closedDrill.DrillItems.Where(c => Math.Abs((c.CompletedDate.Value - processDate).Ticks) == nearestDiff).First();
+        nearest.DueDate = processDate;
+        // add to usedTicks
+        usedTicks.Add(nearestDiff);
+        // NOTE: This is from the sched even calculator
+        switch (schedule.SchedInterval)
+        {
+            case ScheduleTask.Interval.Monthly:
+                processDate = processDate.AddMonths(1);
+                break;
+        }
+        Console.WriteLine(JsonSerializer.Serialize(nearest));
+        Console.WriteLine("------");
+    }
+
+}
+
+/// <summary>
+/// Get the nearest date from a range of dates.
+/// </summary>
+/// <param name="dateTime">The target date.</param>
+/// <param name="dateTimes">The range of dates to find the nearest date from.</param>
+/// <returns>The nearest date to the given target date.</returns>
+static DateTime GetNearestDate(DateTime dateTime, params DateTime[] dateTimes)
+{
+    return dateTime.Add(dateTimes.Min(d => (d - dateTime).Duration()));
+}
+
